a78f9e2ba13f4f6bae7dbec9b80b11eb000a88c8
Teyras <teyras@gmail.com>
2014-04-18 21:21:12 +0200

    added IPv6 manipulation tests

--- tests/utils-basic.test.c
+++ tests/utils-basic.test.c
@@ -39,8 +39,27 @@ int main (int argc, char **argv)
 	addr_mask(&ss_net, &ss_mask);
 	assert_ip("192.0.0.0", &ss_net, "IPv4 masking");
 	
-	assert_int(1, addr_match(&ss_addr, &ss_mask, &ss_mask), "IPv4 matching subnet");
-	assert_int(0, addr_match(&ss_addr2, &ss_mask, &ss_mask), "IPv4 non-matching subnet");
+	assert_int(1, addr_match(&ss_addr, &ss_net, &ss_mask), "IPv4 matching subnet");
+	assert_int(0, addr_match(&ss_addr2, &ss_net, &ss_mask), "IPv4 non-matching subnet");
+	
+	
+	ss_addr.ss_family = AF_INET6;
+	inet_pton(AF_INET6, "2001:db8::1428:57ab", &((struct sockaddr_in6 *) &ss_addr)->sin6_addr);
+	
+	ss_addr2.ss_family = AF_INET6;
+	inet_pton(AF_INET6, "2002:db8::1428:57ab", &((struct sockaddr_in6 *) &ss_addr2)->sin6_addr);
+	
+	ss_net.ss_family = AF_INET6;
+	inet_pton(AF_INET6, "2001::cdef", &((struct sockaddr_in6 *) &ss_net)->sin6_addr);
+	
+	ss_mask.ss_family = AF_INET6;
+	inet_pton(AF_INET6, "ffff::", &((struct sockaddr_in6 *) &ss_mask)->sin6_addr);
+	
+	addr_mask(&ss_net, &ss_mask);
+	assert_ip6("2001::", &ss_net, "IPv6 masking");
+	
+	assert_int(1, addr_match(&ss_addr, &ss_net, &ss_mask), "IPv6 matching subnet");
+	assert_int(0, addr_match(&ss_addr2, &ss_net, &ss_mask), "IPv6 non-matching subnet");
 	
 	fct_send();
 	return 0;
