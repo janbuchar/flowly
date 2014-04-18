0c3362e406981faec0f66b3ac7f3045610c10ea9
Teyras <Teyras@gmail.com>
2014-04-18 15:41:56 +0200

    config test cleanup

--- tests/config-basic.test.c
+++ tests/config-basic.test.c
@@ -4,6 +4,20 @@
 #include "../src/list.c"
 #include "../src/config.c"
 
+void assert_ip (char *expected, struct sockaddr_storage *reality, char *msg)
+{
+	char dst[255];
+	inet_ntop(AF_INET, &((struct sockaddr_in *) reality)->sin_addr, &dst[0], 255);
+	assert_string(expected, dst, msg);
+}
+
+void assert_ip6 (char *expected, struct sockaddr_storage *reality, char *msg)
+{
+	char dst[255];
+	inet_ntop(AF_INET6, &((struct sockaddr_in6 *) reality)->sin6_addr, &dst[0], 255);
+	assert_string(expected, dst, msg);
+}
+
 int main (int argc, char **argv)
 {
 	fct_init();
@@ -15,39 +29,36 @@ int main (int argc, char **argv)
 	
 	assert_int(rc, 0, "config_load() return value");
 	
-	assert_int(config.network_count, 2, "Correct network grouping");
-	assert_int(config.route_count, 3, "Route loading");
-	assert_int(config.client_count, 3, "Client loading");
+	assert_int(2, config.network_count, "Correct network grouping");
+	assert_int(3, config.route_count, "Route loading");
+	assert_int(3, config.client_count, "Client loading");
 	
-	assert_string(config.listen_port, "6666", "Port loading");
-	assert_int(config.send_interval, 6666, "Sending interval parsing");
+	assert_string("6666", config.listen_port, "Port loading");
+	assert_int(6666, config.send_interval, "Sending interval parsing");
 	
-	assert_int(config.routes[0].net_id, 0, "Route #1: net_id");
-	assert_string(inet_ntop(AF_INET, &((struct sockaddr_in *) &config.routes[0].addr)->sin_addr, &dst, 255), 
-		"8.8.4.4", "Route #1: address");
+	assert_int(0, config.routes[0].net_id, "Route #1: net_id");
+	assert_ip("8.8.4.4", &config.routes[0].addr, "Route #1: address");
+	assert_ip("255.255.0.0", &config.routes[0].mask, "Route #1: mask");
 	
-	assert_int(config.routes[1].net_id, 0, "Route #2: net_id");
-	assert_string(inet_ntop(AF_INET, &((struct sockaddr_in *) &config.routes[1].addr)->sin_addr, &dst, 255), 
-		"8.8.6.6", "Route #2: address");
+	assert_int(0, config.routes[1].net_id, "Route #2: net_id");
+	assert_ip("8.8.6.6", &config.routes[1].addr, "Route #2: address");
+	assert_ip("255.0.0.0", &config.routes[1].mask, "Route #2: mask");
 	
-	assert_int(config.routes[2].net_id, 1, "Route #3: net_id");
-	assert_string(inet_ntop(AF_INET, &((struct sockaddr_in *) &config.routes[2].addr)->sin_addr, &dst, 255), 
-		"10.0.0.138", "Route #3: address");
+	assert_int(1, config.routes[2].net_id, "Route #3: net_id");
+	assert_ip("10.0.0.138", &config.routes[2].addr, "Route #3: address");
+	assert_ip("255.255.255.0", &config.routes[2].mask, "Route #3: mask");
 	
-	assert_string(config.networks[0].name, "network1", "Network #1: name");
-	assert_string(config.networks[1].name, "network2", "Network #2: name");
+	assert_string("network1", config.networks[0].name, "Network #1: name");
+	assert_string("network2", config.networks[1].name, "Network #2: name");
 	
-	assert_int(config.clients[0].format, RAW, "Client #1: format");
-	assert_string(inet_ntop(AF_INET, &((struct sockaddr_in *) &config.clients[0].addr)->sin_addr, &dst, 255), 
-		"127.0.0.1", "Client #1: address");
+	assert_int(RAW, config.clients[0].format, "Client #1: format");
+	assert_ip("127.0.0.1", &config.clients[0].addr, "Client #1: address");
 	
-	assert_int(config.clients[1].format, RAW, "Client #2: format");
-	assert_string(inet_ntop(AF_INET, &((struct sockaddr_in *) &config.clients[1].addr)->sin_addr, &dst, 255), 
-		"192.168.0.1", "Client #2: address");
+	assert_int(RAW, config.clients[1].format, "Client #2: format");
+	assert_ip("192.168.0.1", &config.clients[1].addr, "Client #2: address");
 	
-	assert_int(config.clients[2].format, RAW, "Client #3: format");
-	assert_string(inet_ntop(AF_INET6, &((struct sockaddr_in6 *) &config.clients[2].addr)->sin6_addr, &dst, 255), 
-		"2001:db8::1428:57ab", "Client #3: address");
+	assert_int(RAW, config.clients[2].format, "Client #3: format");
+	assert_ip6("2001:db8::1428:57ab", &config.clients[2].addr, "Client #3: address");
 	
 	config_free(&config);
 	fct_send();
