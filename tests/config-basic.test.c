a99826f9b27231d95af56c69d206304af474b705
Teyras <teyras@gmail.com>
2014-04-19 14:34:59 +0200

    include assert_ip.c in tests

--- tests/config-basic.test.c
+++ tests/config-basic.test.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
+#include "assert_ip.c"
 #include "../src/list.c"
 #include "../src/config.c"
 #include "../src/utils.c"
-
-void assert_ip (char *expected, struct sockaddr_storage *reality, char *msg)
-{
-	char dst[255];
-	inet_ntop(AF_INET, &((struct sockaddr_in *) reality)->sin_addr, &dst[0], 255);
-	assert_string(expected, dst, msg);
-}
-
-void assert_ip6 (char *expected, struct sockaddr_storage *reality, char *msg)
-{
-	char dst[255];
-	inet_ntop(AF_INET6, &((struct sockaddr_in6 *) reality)->sin6_addr, &dst[0], 255);
-	assert_string(expected, dst, msg);
-}
 
 int main (int argc, char **argv)
 {
--- tests/utils-basic.test.c
+++ tests/utils-basic.test.c
@@ -1,20 +1,7 @@
 #include <arpa/inet.h>
 
+#include "assert_ip.c"
 #include "../src/utils.c"
-
-void assert_ip (char *expected, struct sockaddr_storage *reality, char *msg)
-{
-	char dst[255];
-	inet_ntop(AF_INET, &((struct sockaddr_in *) reality)->sin_addr, &dst[0], 255);
-	assert_string(expected, dst, msg);
-}
-
-void assert_ip6 (char *expected, struct sockaddr_storage *reality, char *msg)
-{
-	char dst[255];
-	inet_ntop(AF_INET6, &((struct sockaddr_in6 *) reality)->sin6_addr, &dst[0], 255);
-	assert_string(expected, dst, msg);
-}
 
 int main (int argc, char **argv)
 {
