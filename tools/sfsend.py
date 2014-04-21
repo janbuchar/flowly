1320b1ff155b999a576088032c5aafaef387d7e9
Teyras <teyras@gmail.com>
2014-04-21 21:51:28 +0200

    added convenience functions to sfsend

--- tools/sfsend.py
+++ tools/sfsend.py
@@ -10,3 +10,30 @@ def get_addr (addr, port):
 
 def create_socket (family):
 	return socket.socket(family, socket.SOCK_DGRAM)
+
+def send_single (addr, port, data):
+	"""
+	Send a single packet
+	
+	Args:
+		addr: a string representation of destination address
+		port: destination port
+		data: data to be sent
+	"""
+	family, sockaddr = get_addr(addr, port)
+	with create_socket(family) as sock:
+		send_data(sock, sockaddr, data)
+
+def send_list (addr, port, data):
+	"""
+	Send every item of an iterable as a packet
+	
+	Args:
+		addr: a string representation of destination address
+		port: destination port
+		data: list of items to be sent
+	"""
+	family, sockaddr = get_addr(addr, port)
+	with create_socket(family) as sock:
+		for item in data:
+			send_data(sock, sockaddr, item)
