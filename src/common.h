f45ca240f333fa89a3f36d767436f07980457ae5
Jan Buchar <Teyras@gmail.com>
2014-04-23 15:36:39 +0200

    added output structure prototype

--- src/common.h
+++ src/common.h
@@ -1,9 +1,10 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-#define NET_NAME_LENGTH 64
 #define MAX_SFLOW_PACKET_SIZE 65536
 #define STAT_COUNT 255
+#define STAT_NAME_LENGTH 64
+#define NET_NAME_LENGTH 64
 
 typedef enum {
 	IN,
--- src/output.h
+++ src/output.h
@@ -1,8 +1,27 @@
 #ifndef _OUTPUT_H_
 #define _OUTPUT_H_
 
+#include <sys/types.h>
+#include <time.h>
+
+#include "common.h"
 #include "config.h"
 #include "flowstat.h"
+
+#define DIRECTION_IN 'I'
+#define DIRECTION_OUT 'O'
+
+typedef struct {
+	stat_number_t item_count;
+	struct timespec time;
+} output_header_t;
+
+typedef struct {
+	char direction;
+	char network[NET_NAME_LENGTH];
+	char name[STAT_NAME_LENGTH];
+	stat_number_t value;
+} output_item_t;
 
 void
 output (flowly_config_t *config, stat_container_t *stats);
