bcf6bf5f299f369cd1d48c577147abcf068ad125
Teyras <teyras@gmail.com>
2014-04-27 23:49:47 +0200

    partially updated client.py for new output format

--- tools/client.py
+++ tools/client.py
@@ -18,19 +18,29 @@ class flowly_struct:
 		return struct.calcsize(cls.fmt)
 
 class flowly_header (flowly_struct):
-	fmt = "!lll"
+	fmt = "!lllll"
 	def __init__ (self, data):
-		self.count = data[0]
+		self.version = data[0]
 		self.time = data[1]
 		self.nanotime = data[2]
+		self.network_count = data[3]
+		self.stat_count = data[4]
 
-class flowly_stat (flowly_struct):
-	fmt = "!c63s64sQ"
+class flowly_stat_header (flowly_struct):
+	fmt = "!64s"
+	def __init__ (self, data):
+		self.name = data[0]
+
+class flowly_network_header (flowly_struct):
+	fmt = "!64s"
+	def __init (self, data):
+		self.name = data[0]
+
+class flowly_item (flowly_struct):
+	fmt = "!QQ"
 	def __init__(self, data):
-		self.direction = data[0].decode()
-		self.network = c_str(data[1])
-		self.name = c_str(data[2])
-		self.value = data[3]
+		self.value_in = data[0]
+		self.value_out = data[1]
 
 if (len(sys.argv) < 3):
 	print("usage: client.py [address] [port]")
@@ -44,10 +54,21 @@ with socket.socket(family, socket.SOCK_DGRAM) as sock:
 	
 	while True:
 		data, remote_addr = sock.recvfrom(65536)
-		header = flowly_header(struct.unpack(flowly_header.fmt, data[:flowly_header.size()]))
-		items = map(flowly_stat, struct.iter_unpack(flowly_stat.fmt, data[flowly_header.size() : flowly_header.size() + header.count * flowly_stat.size()]))
+		end = flowly_header.size()
+		header = flowly_header(struct.unpack(flowly_header.fmt, data[:end]))
 		
-		print("Received {0.count} items (time {0.time}):".format(header))
+		if (header.version != 0): # TODO upgrade to 1
+			print("Unsupported flowly protocol version")
+			sys.exit(1)
+		
+		stats = map(flowly_stat_header, struct.iter_unpack(flowly_stat_header.fmt, data[end : end + header.stat_count * flowly_stat_header.size()]))
+		end += header.stat_count * flowly_stat_header.size()
+		
+		def items ():
+			for i in range(header.network_count):
+				yield i
+		
+		print("Received {0.network_count} items (time {0.time}):".format(header))
 		
 		for item in items:
 			print("{0.direction} {0.network}\n\t {0.name} = {0.value}".format(item))
